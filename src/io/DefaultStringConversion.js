8fbaa9b3807824a4a309ceed131f6eecf5abd009
arolson101 <andrew@olsononline.org>
2015-01-03 17:35:15 -0600

    remove moment.js

--- src/io/DefaultStringConversion.js
+++ src/io/DefaultStringConversion.js
@@ -20,7 +20,6 @@ var Status = require("../domain/data/common/Status");
 var StatusCode = require("../domain/data/common/StatusCode");
 var UnknownStatusCode = require("../domain/data/common/UnknownStatusCode");
 var StringConversion = require("./StringConversion");
-var moment = require("moment");
 
 /**
  * Utility class for conversion to/from OFX strings.
@@ -28,20 +27,10 @@ var moment = require("moment");
  * @class
  */
 function DefaultStringConversion () {
-
-//  /**
-//   * @name DefaultStringConversion#GMT_TIME_ZONE
-//   * @type TimeZone
-//   */
-//  this.GMT_TIME_ZONE = TimeZone.getTimeZone("GMT");
 }
 
+
 inherit(DefaultStringConversion, "implements", StringConversion);
-
-//var DATE_FORMAT = "YYYYMMDDHHmmss.SSS[Z]";
-//var DATE_FORMAT_LENGTH = "yyyyMMddHHmmss.SSS".length;
-//var TIME_FORMAT_LENGTH = "HHmmss.SSS".length;
-
 
 
 DefaultStringConversion.prototype.toString = function(/*Object*/ value) {
@@ -86,9 +75,6 @@ DefaultStringConversion.prototype.fromString = function(/*Class<E>*/ clazz, /*St
   else if (inherit.isAssignableFrom(Boolean, clazz)) {
     return "Y" === value.toUpperCase();
   }
-//  else if (Time.class.isAssignableFrom(clazz)) {
-//    return parseTime(value);
-//  }
   else if (inherit.isAssignableFrom(Date, clazz)) {
     return this.parseDate(value);
   }
@@ -150,10 +136,6 @@ function pad(num, size) {
  * @return {String} The date format.
  */
 DefaultStringConversion.prototype.formatDate = function(date) {
-//  GregorianCalendar calendar = new GregorianCalendar(GMT_TIME_ZONE);
-//  calendar.setTime(date);
-//  return String.format("%1$tY%1$tm%1$td%1$tH%1$tM%1$tS.%1$tL", calendar);
-  
   return pad(date.getFullYear(), 4) +
     pad(date.getMonth() + 1, 2) +
     pad(date.getDay(), 2) +
@@ -166,72 +148,6 @@ DefaultStringConversion.prototype.formatDate = function(date) {
     (date.getTimezoneOffset() / 60) +
     "]";
 };
-
-
-/**
- * Parses a time according to OFX.
- *
- * @param {String} value The value of the date.
- * @return {Time} The date value.
- */
-DefaultStringConversion.prototype.parseTime = function(value) {
-//  var parseableTime = new char[TIME_FORMAT_LENGTH];
-//  Arrays.fill(parseableTime, '0');
-//  parseableTime[parseableTime.length - 4] = '.';
-//  value.getChars(0, Math.min(parseableTime.length, value.length), parseableTime, 0);
-//
-//  int hour = Integer.parseInt(new String(parseableTime, 0, 2));
-//  int minute = Integer.parseInt(new String(parseableTime, 2, 2));
-//  int second = Integer.parseInt(new String(parseableTime, 4, 2));
-//  int milli = Integer.parseInt(new String(parseableTime, 7, 3));
-//
-//  //set up a new calendar at zero, then set all the fields.
-//  GregorianCalendar calendar = new GregorianCalendar(0, 0, 0, hour, minute, second);
-//  if (value.length > parseableTime.length) {
-//    String tzoffset = value.substring(parseableTime.length);
-//    calendar.setTimeZone(parseTimeZone(tzoffset));
-//  }
-//  else {
-//    calendar.setTimeZone(GMT_TIME_ZONE);
-//  }
-//  calendar.add(GregorianCalendar.MILLISECOND, milli);
-//
-//  return new Time(calendar.getTimeInMillis());
-  return Date.parse(value);
-};
-
-
-/**
- * Format the time according to the OFX spec.
- *
- * @param {Time} time The time to format.
- * @return {String} The formatted time.
- */
-DefaultStringConversion.prototype.formatTime = function(time) {
-//  GregorianCalendar calendar = new GregorianCalendar(GMT_TIME_ZONE);
-//  calendar.setTime(time);
-//  return String.format("%1$tH%1$tM%1$tS.%1$tL", calendar);
-  return moment(time).format("YYYYMMDDHHmmss.SSS[Z]");
-  //return time.toTimeString();
-};
-
-
-///**
-// * Parse the timezone offset of the form [HOURS_OFF_GMT:TZ_ID]
-// *
-// * @param {String} tzoffset The offset pattern.
-// * @return {TimeZone} The timezone.
-// */
-//DefaultStringConversion.prototype.parseTimeZone = function(tzoffset) {
-//  StringTokenizer tokenizer = new StringTokenizer(tzoffset, "[]:");
-//  TimeZone tz = GMT_TIME_ZONE;
-//  if (tokenizer.hasMoreTokens()) {
-//    String hoursOff = tokenizer.nextToken();
-//    tz = TimeZone.getTimeZone("GMT" + hoursOff);
-//  }
-//
-//  return tz;
-//};
 
 
 
