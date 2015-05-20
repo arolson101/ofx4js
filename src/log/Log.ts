5ce8557c3e99e138d9174dd14c21959a4d378598
arolson101 <andrew@olsononline.org>
2015-05-19 22:26:59 -0500

    missing log sources

---
+++ src/log/Log.ts
@@ -0,0 +1,63 @@
+
+module ofx4js.log {
+
+	
+export class Log {
+	private infoEnabled: boolean;
+	private debugEnabled: boolean;
+	
+	constructor() {
+		this.infoEnabled = false;
+		this.debugEnabled = false;
+	}
+
+	public setInfoEnabled(value: boolean): void {
+		this.infoEnabled = value;
+	}
+
+	public isInfoEnabled(): boolean {
+		return this.infoEnabled;
+	}
+	
+	public info(...texts: Array<string>): void {
+		if(this.isInfoEnabled()) {
+			console.log(texts);
+		}
+	}
+
+	public setDebugEnabled(value: boolean): void {
+		this.debugEnabled = value;
+	}
+
+	public isDebugEnabled(): boolean {
+		return this.debugEnabled;
+	}
+	
+	public debug(...texts: Array<string>): void {
+		if(this.isDebugEnabled()) {
+			console.log(texts);
+		}
+	}
+	
+	public warning(...texts: Array<string>): void {
+		console.log(texts);
+	}
+	
+	public error(...texts: Array<string>): void {
+		console.log(texts);
+	}
+}
+
+
+export class LogFactory {
+	
+	static getLog(clazz: any) {
+		if(!clazz.Log) {
+			clazz.Log = new Log();
+		}
+		
+		return clazz.Log;
+	}
+}
+
+}
