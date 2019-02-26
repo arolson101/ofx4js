e340122f9f2bc0d75071b651219dec3b909e1735
arolson101 <andrew@olsononline.org>
2019-02-26 16:29:52 -0600

    switch to cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,26 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import { terser } from "rollup-plugin-terser";
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
 
-import pkg from './package.json';
+import pkg from './package.json'
 
 export default [
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/index.ts',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'esm', sourcemap: true }
-		],
-		plugins: [
-      resolve({preferBuiltins: false}), // so Rollup can find `ms`
-			typescript(/*{ plugin options }*/),
-			terser(),
-		],
-		external: Object.keys(pkg.dependencies)
-	}
-];
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    external: ['ms'],
+    output: [{ file: pkg.main, format: 'cjs', sourcemap: true }],
+    plugins: [
+      resolve({ preferBuiltins: false }), // so Rollup can find `ms`
+      typescript(/*{ plugin options }*/),
+      terser()
+    ],
+    external: Object.keys(pkg.dependencies)
+  }
+]
